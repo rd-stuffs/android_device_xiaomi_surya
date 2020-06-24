5864635f07092424fdf1fabdd901930c19e9bb90
Jyotiraditya <jyotiraditya@aospa.co>
2020-06-24 11:02:21 +0200

    surya: parts: Give device settings its own fragment
    Change-Id: I817f1dccb3d348473fd230fbc390af2b9aed6374

---
+++ parts/src/org/lineageos/settings/MainSettingsActivity.java
@@ -0,0 +1,19 @@
+package org.lineageos.settings;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class MainSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, new MainSettingsFragment())
+                .commit();
+    }
+
+}
---
+++ parts/src/org/lineageos/settings/MainSettingsFragment.java
@@ -0,0 +1,70 @@
+package org.lineageos.settings;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+
+import org.lineageos.settings.dirac.DiracActivity;
+import org.lineageos.settings.speaker.ClearSpeakerActivity;
+import org.lineageos.settings.display.LcdFeaturesPreferenceActivity;
+import org.lineageos.settings.refreshrate.RefreshActivity;
+import org.lineageos.settings.thermal.ThermalSettingsActivity;
+
+public class MainSettingsFragment extends PreferenceFragment {
+
+    private static final String PREF_DIRAC_SETTINGS = "dirac_settings";
+    private static final String PREF_CLEAR_SPEAKER_SETTINGS = "clear_speaker_settings";
+    private static final String PREF_LCD_FEATURES_SETTINGS = "lcd_features_settings";
+    private static final String PREF_REFRESH_RATE_SETTINGS = "refresh_rate_settings";
+    private static final String PREF_THERMAL_SETTINGS = "thermal_settings";
+
+    private Preference mDiracSettingsPref;
+    private Preference mClearSpeakerSettingsPref;
+    private Preference mLcdFeaturesSettingsPref;
+    private Preference mRefreshRateSettingsPref;
+    private Preference mThermalSettingsPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.device_settings);
+
+        mDiracSettingsPref = (Preference) findPreference(PREF_DIRAC_SETTINGS);
+        mDiracSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), DiracActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        mClearSpeakerSettingsPref = (Preference) findPreference(PREF_CLEAR_SPEAKER_SETTINGS);
+        mClearSpeakerSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), ClearSpeakerActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        mLcdFeaturesSettingsPref = (Preference) findPreference(PREF_LCD_FEATURES_SETTINGS);
+        mLcdFeaturesSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), LcdFeaturesPreferenceActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        mRefreshRateSettingsPref = (Preference) findPreference(PREF_REFRESH_RATE_SETTINGS);
+        mRefreshRateSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), RefreshActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        mThermalSettingsPref = (Preference) findPreference(PREF_THERMAL_SETTINGS);
+        mThermalSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), ThermalSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+    }
+
+}
