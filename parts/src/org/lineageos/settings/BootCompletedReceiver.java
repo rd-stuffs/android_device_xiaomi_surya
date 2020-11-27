f051cb3d3e37d04633aaa903a28567625083eec0
daniml3 <danimoral1001@gmail.com>
2020-11-27 17:38:14 +0100

    surya: parts: Initial KCAL settings implementation
    * Added RGB, saturation and contrast settings
    * Added a preference for restoring default values
    
    [ghostrider-reborn: Move it to our Xiaomi Parts fragment under new category]
    
    Change-Id: Id58c9fba01909807664c1c7ec05b8b894d468481

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,12 +20,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 
 import android.util.Log;
 
+import androidx.preference.PreferenceManager;
+
 import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.display.KcalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -34,6 +40,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
 
         if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
@@ -44,6 +51,16 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Dirac
         DiracUtils.onBootCompleted(context);
+
+        // KCAL
+        FileUtils.writeLine(KcalUtils.KCAL_ENABLE_NODE,
+            sharedPrefs.getBoolean("kcal_enable", false) ? "1" : "0");
+
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 1, sharedPrefs.getInt("red_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 2, sharedPrefs.getInt("green_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_RGB_NODE, 3, sharedPrefs.getInt("blue_slider", 256));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_SATURATION_NODE, 0, sharedPrefs.getInt("saturation_slider", 255));
+        KcalUtils.writeConfigToNode(KcalUtils.KCAL_CONTRAST_NODE, 0, sharedPrefs.getInt("contrast_slider", 255));
 
         // Refresh Rate
         RefreshUtils.startService(context);
--- parts/src/org/lineageos/settings/MainSettingsFragment.java
+++ parts/src/org/lineageos/settings/MainSettingsFragment.java
@@ -8,6 +8,7 @@ import androidx.preference.PreferenceFragment;
 
 import org.lineageos.settings.dirac.DiracActivity;
 import org.lineageos.settings.speaker.ClearSpeakerActivity;
+import org.lineageos.settings.display.KcalSettingsActivity;
 import org.lineageos.settings.display.LcdFeaturesPreferenceActivity;
 import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.thermal.ThermalSettingsActivity;
@@ -16,12 +17,14 @@ public class MainSettingsFragment extends PreferenceFragment {
 
     private static final String PREF_DIRAC_SETTINGS = "dirac_settings";
     private static final String PREF_CLEAR_SPEAKER_SETTINGS = "clear_speaker_settings";
+    private static final String PREF_KCAL_SETTINGS = "kcal_settings";
     private static final String PREF_LCD_FEATURES_SETTINGS = "lcd_features_settings";
     private static final String PREF_REFRESH_RATE_SETTINGS = "refresh_rate_settings";
     private static final String PREF_THERMAL_SETTINGS = "thermal_settings";
 
     private Preference mDiracSettingsPref;
     private Preference mClearSpeakerSettingsPref;
+    private Preference mKcalSettingsPref;
     private Preference mLcdFeaturesSettingsPref;
     private Preference mRefreshRateSettingsPref;
     private Preference mThermalSettingsPref;
@@ -40,6 +43,13 @@ public class MainSettingsFragment extends PreferenceFragment {
         mClearSpeakerSettingsPref = (Preference) findPreference(PREF_CLEAR_SPEAKER_SETTINGS);
         mClearSpeakerSettingsPref.setOnPreferenceClickListener(preference -> {
             Intent intent = new Intent(getActivity().getApplicationContext(), ClearSpeakerActivity.class);
+            startActivity(intent);
+            return true;
+        });
+
+        mKcalSettingsPref = (Preference) findPreference(PREF_KCAL_SETTINGS);
+        mKcalSettingsPref.setOnPreferenceClickListener(preference -> {
+            Intent intent = new Intent(getActivity().getApplicationContext(), KcalSettingsActivity.class);
             startActivity(intent);
             return true;
         });
