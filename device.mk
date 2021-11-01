a3305fbc728967194fa1fe4224e3a0c6d1dc9367
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-01 17:38:02 +0530

    surya: Drop keyhandler for fingerprint sensor
     * kinda buggy on android 12, and now we have an alternative
       fix from the fpc kernel driver
    
    https://github.com/ghostrider-reborn/android_kernel_xiaomi_surya/commit/1871390cd2c33f27862f10f08017b81b95779ff9

--- device.mk
+++ device.mk
@@ -146,10 +146,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Keyhandler
-PRODUCT_PACKAGES += \
-    KeyHandler
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
