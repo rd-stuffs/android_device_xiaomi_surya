f38c6e80cfed797c128adb30d7a6a15b0e6bf09e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-24 00:27:24 +0530

    surya: Disable ART debug and optimize dexpreopt
     * don't include debug configs in art
     * use everything filter for dexpreopt (https://github.com/AOSPA/android_vendor_pa/commit/ab5061c425e61bc7d36f7bcf480feea975795545)
     * use non-debug dex2oat
    
    Change-Id: Ic310df1e2c466437b26fcb2e5ab9333694328c48

--- device.mk
+++ device.mk
@@ -36,6 +36,11 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Dex/ART optimization
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
+USE_DEX2OAT_DEBUG := false
+
 # Display
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
