2728a0a235313de3da83ed610ddaf16d3e6eafbd
Richard Raya <rdxzv.dev@gmail.com>
2022-11-02 22:14:07 -0300

    surya: Update android.hardware.drm to 1.4
    Change-Id: I902b66bb9998c803535942ad252a147bdb128702

--- device.mk
+++ device.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 # Filesystems
