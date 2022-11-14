b6620c99ed1006720d5d6aab69e6a011c40440ef
Richard Raya <rdxzv.dev@gmail.com>
2022-11-14 20:36:22 -0300

    surya: power: Cleanup
    Change-Id: I8edb455506dcebc2b2b0405e1f053d06e84663ec

--- power-libperfmgr/Android.mk
+++ power-libperfmgr/Android.mk
@@ -45,15 +45,6 @@ LOCAL_SRC_FILES := \
 
 LOCAL_CFLAGS := -Wno-unused-parameter -Wno-unused-variable
 
-ifneq ($(TARGET_POWERHAL_MODE_EXT),)
-    LOCAL_CFLAGS += -DMODE_EXT
-    LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_MODE_EXT)
-endif
-
-ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
-    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
-endif
-
 LOCAL_MODULE := android.hardware.power-service.xiaomi-libperfmgr
 LOCAL_INIT_RC := android.hardware.power-service.xiaomi-libperfmgr.rc
 LOCAL_MODULE_TAGS := optional
--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -41,11 +41,6 @@ namespace pixel {
 using ::aidl::google::hardware::power::impl::pixel::PowerHintSession;
 using ::android::perfmgr::HintManager;
 
-#ifdef MODE_EXT
-extern bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return);
-extern bool setDeviceSpecificMode(Mode type, bool enabled);
-#endif
-
 constexpr char kPowerHalStateProp[] = "vendor.powerhal.state";
 constexpr char kPowerHalAudioProp[] = "vendor.powerhal.audio";
 constexpr char kPowerHalRenderingProp[] = "vendor.powerhal.rendering";
@@ -84,17 +79,7 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
         HintManager::GetInstance()->GetAdpfProfile()->mReportingRateLimitNs > 0) {
         PowerSessionManager::getInstance()->updateHintMode(toString(type), enabled);
     }
-#ifdef MODE_EXT
-    if (setDeviceSpecificMode(type, enabled)) {
-        return ndk::ScopedAStatus::ok();
-    }
-#endif
     switch (type) {
-#ifdef TAP_TO_WAKE_NODE
-        case Mode::DOUBLE_TAP_TO_WAKE:
-            ::android::base::WriteStringToFile(enabled ? "1" : "0", TAP_TO_WAKE_NODE, true);
-            break;
-#endif
         case Mode::SUSTAINED_PERFORMANCE:
             if (enabled) {
                 HintManager::GetInstance()->DoHint("SUSTAINED_PERFORMANCE");
@@ -106,10 +91,8 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
                 break;
             }
             [[fallthrough]];
-#ifndef TAP_TO_WAKE_NODE
         case Mode::DOUBLE_TAP_TO_WAKE:
             [[fallthrough]];
-#endif
         case Mode::FIXED_PERFORMANCE:
             [[fallthrough]];
         case Mode::EXPENSIVE_RENDERING:
@@ -135,18 +118,8 @@ ndk::ScopedAStatus Power::setMode(Mode type, bool enabled) {
 }
 
 ndk::ScopedAStatus Power::isModeSupported(Mode type, bool *_aidl_return) {
-#ifdef MODE_EXT
-    if (isDeviceSpecificModeSupported(type, _aidl_return)) {
-        return ndk::ScopedAStatus::ok();
-    }
-#endif
 
     bool supported = HintManager::GetInstance()->IsHintSupported(toString(type));
-#ifdef TAP_TO_WAKE_NODE
-    if (type == Mode::DOUBLE_TAP_TO_WAKE) {
-        supported = true;
-    }
-#endif
     LOG(INFO) << "Power mode " << toString(type) << " isModeSupported: " << supported;
     *_aidl_return = supported;
     return ndk::ScopedAStatus::ok();
