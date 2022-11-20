84be12fb358d0a468592485f98d6df40faf0ce9a
justinweng <justinweng@google.com>
2022-11-20 11:19:02 -0300

    surya: Extend buffer size to 256kb for offload playback
    Extend buffer size for offload playback to reduce the frequency of CPU
    wake up.
    It makes ADSP to process more data one time and reduce the number of
    times to wake up CPU.
    
    Bug: 137973303
    Test: MP3 playback
    Change-Id: I39fb8130b585eaaf3324d360210972d26e9fd0c6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
