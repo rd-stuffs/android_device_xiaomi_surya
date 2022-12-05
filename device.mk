885de6b9d6753b4d45140ea077d50655fbdeb7f1
Jake Weinstein <jake@aospa.co>
2022-12-05 18:51:17 +0900

    surya: Make VRR more conservative
    Change-Id: I6eb3aebf0de1de443688d5949f8082214af7bd42
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -155,8 +155,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.frame_rate_multiple_threshold=120 \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=700 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true \
     vendor.display.idle_time=0 \
