4f4f6139ee1c796a59a3813a9bd3dba97a4c30a9
Richard Raya <rdxzv.dev@gmail.com>
2023-01-03 19:28:48 -0300

    surya: parts: Move device-specific settings to a proper place
    Change-Id: Ia45508075f69e279c54005493b7534515c0d71d0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,7 +110,6 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/props/system_ext.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/props/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
