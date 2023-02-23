b0d0c3677e83a428eb26dbbfaf561d4b71544a37
Richard Raya <rdxzv.dev@gmail.com>
2023-02-23 01:05:05 -0300

    surya: parts: Rename extreme to maximum refresh rate
    Change-Id: I66a795ac65bf61c7fbcc98949d505ffbb184ccfa

--- parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshSettingsFragment.java
@@ -194,7 +194,7 @@ public class RefreshSettingsFragment extends PreferenceFragment
                 return R.drawable.ic_refresh_60;
             case RefreshUtils.STATE_HIGH:
                 return R.drawable.ic_refresh_90;
-            case RefreshUtils.STATE_EXTREME:
+            case RefreshUtils.STATE_MAXIMUM:
                 return R.drawable.ic_refresh_120;
             case RefreshUtils.STATE_DEFAULT:
             default:
@@ -229,7 +229,7 @@ public class RefreshSettingsFragment extends PreferenceFragment
                 R.string.refresh_low,
                 R.string.refresh_standard,
                 R.string.refresh_high,
-                R.string.refresh_extreme
+                R.string.refresh_maximum
         };
 
         private ModeAdapter(Context context) {
