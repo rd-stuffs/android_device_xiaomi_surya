5c45359741b8c0fc5f8a474fb5331b69f6a792bd
Richard Raya <rdxzv.dev@gmail.com>
2023-04-14 16:49:19 -0300

    surya: parts: Rename to XiaomiParts
    Change-Id: I1fca735a8292abb983e943f981e0a869a12373ee

--- parts/src/org/lineageos/settings/MainSettingsFragment.java
+++ parts/src/org/lineageos/settings/MainSettingsFragment.java
@@ -28,7 +28,7 @@ public class MainSettingsFragment extends PreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.device_settings);
+        addPreferencesFromResource(R.xml.xiaomiparts);
 
         mDiracSettingsPref = (Preference) findPreference(PREF_DIRAC_SETTINGS);
         mDiracSettingsPref.setOnPreferenceClickListener(preference -> {
