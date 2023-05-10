638c5e8f02b05da5850466cd21eac49d940b9e9a
Richard Raya <rdxzv.dev@gmail.com>
2023-05-10 17:18:54 -0300

    surya: parts: Allow to log received boot completed intent
    Change-Id: I69165293af355a42824b8d39c48983ef6d69c305

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -21,17 +21,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 
+import android.util.Log;
+
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
+    private static final boolean DEBUG = false;
+    private static final String TAG = "XiaomiParts";
+
     @Override
     public void onReceive(final Context context, Intent intent) {
+
         if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             return;
         }
+
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
 
         // Dirac
         DiracUtils.onBootCompleted(context);
