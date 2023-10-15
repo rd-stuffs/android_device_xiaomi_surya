cec734289a71519f6582b0c5bb867c2ceabed07c
Richard Raya <rdxzv.dev@gmail.com>
2023-10-15 15:22:55 -0300

    surya: Completely nuke mi_thermald
    Change-Id: I2568349d298eff950be32576e6692fff07c18c16

--- sepolicy/J20C-sepolicy.mk
+++ sepolicy/J20C-sepolicy.mk
@@ -12,8 +12,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/xiaomi/surya/sepolicy/ir/vendor \
     device/xiaomi/surya/sepolicy/fingerprint/vendor \
     device/xiaomi/surya/sepolicy/leds/vendor \
-    device/xiaomi/surya/sepolicy/qcom-extra/vendor \
-    device/xiaomi/surya/sepolicy/thermal/vendor
+    device/xiaomi/surya/sepolicy/qcom-extra/vendor
 
 J20C_SEPOLICY_INCLUDED := true
 endif
