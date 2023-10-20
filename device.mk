f4d93eada1b374c8b4af70b7e0efc94a896bbd29
Richard Raya <rdxzv.dev@gmail.com>
2023-10-20 01:49:18 -0300

    surya: Import powerhint config from sunfish
    https://android.googlesource.com/device/google/sunfish/+/refs/heads/main/powerhint.json HEAD at 53fe4ba5f1845766d015fac0eae8d04d6965d585
    
    Change-Id: I17637e37f14f49a6e73d47383b5fc61663ccd074

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
 # Power
 TARGET_PROVIDES_POWERHAL := true
 PRODUCT_PACKAGES += android.hardware.power-service.xiaomi-libperfmgr
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
