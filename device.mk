5fc072a1de45f18fc39252df8a512097036bc0d2
Richard Raya <rdxzv.dev@gmail.com>
2023-10-24 12:34:08 -0300

    Revert "surya: Drop alarm component"
    This reverts commit 86c3ba90a3e698ea684caf4839a3081299b6a14e.
    
    Change-Id: Iecb14c8174a96575d3242fea4f626acff4bf96f8

--- device.mk
+++ device.mk
@@ -171,6 +171,7 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_O
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
+    alarm \
     audio \
     av \
     bt \
