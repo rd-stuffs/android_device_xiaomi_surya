a5d7e77f7f135680a01f71183c7c51114c69f7b5
Richard Raya <rdxzv.dev@gmail.com>
2023-11-03 23:37:39 -0300

    surya: Wipe packages caches at install time
    Change-Id: I8c4618a8a284e51b2bb1f723d6fab9a6f3ebf6aa
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -24,4 +24,5 @@ def OTA_InstallEnd(info, input_zip):
   AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, input_zip, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+  info.script.AppendExtra('run_program("/system/bin/rm", "-rf", "/data/system/package_cache");')
   return
