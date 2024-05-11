d47d91dd8819d5558547c2fd34767df7f7735c10
Richard Raya <rdxzv.dev@gmail.com>
2024-05-11 00:12:52 -0300

    surya: wifi: Import wpa supplicant conf file
    https://git.codelinaro.org/clo/le/platform/vendor/qcom/wlan/-/blob/LA.UM.9.1.r1-16000-SMxxx0.QSSI14.0/talos/wpa_supplicant_overlay.conf
    
    Change-Id: I2845f80ce4d40b5864bb3644fe5a226f3e2e65f7
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -439,4 +439,5 @@ PRODUCT_COPY_FILES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
