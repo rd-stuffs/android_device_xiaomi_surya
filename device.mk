3c34ad548a8361376e85f0eea6aa4a115fc2faf6
Richard Raya <rdxzv.dev@gmail.com>
2024-05-11 18:42:37 -0300

    surya: Import MiSys blobs from stock
    Change-Id: Ic04cf6e95349946bc7d183a81824d28a68d8f28e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -202,6 +202,11 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0.vendor \
+    android.hidl.memory.block@1.0 \
     android.hidl.memory.block@1.0.vendor
 
 # HVDCP
