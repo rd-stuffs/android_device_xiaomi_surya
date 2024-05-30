00c287a2504036ebd1a6641cefdf44f3dadbd144
Cyber Knight <cyberknight755@gmail.com>
2024-05-30 20:11:45 +0800

    surya: Switch to prebuilt camera provider
    - This is required for Aperture to function properly as logical camera information is broken with OSS camera provider.
    
    [dereference23: This mainly affects Longcheer devices]
    
    Change-Id: I0378b6908be89f3b709a4b4e8f16c804ddb95f09
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -66,7 +66,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
