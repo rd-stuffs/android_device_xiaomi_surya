f5fbc5b5a30651a3686231b88c7f4d306364f153
Richard Raya <rdxzv.dev@gmail.com>
2024-08-02 20:02:53 -0300

    surya: Restore HIDL android.hardware.ir
    Change-Id: I6f6b0c8ab8816ca4c903faeda06964f64c4c7bd1
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -273,7 +273,8 @@ PRODUCT_PACKAGES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.example
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
