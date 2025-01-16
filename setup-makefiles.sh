d9207cd3f3d160c53f668433101cf6a0b6c70158
Richard Raya <rdxzv.dev@gmail.com>
2025-01-15 21:43:09 -0300

    surya: Fix vendor.qti.latency ELF checks
    Change-Id: I7401ee1604e6c49c64e8deb871e0a6be0dd36d92
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -58,6 +58,7 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.hardware.wifidisplaysession@1.0 | \
             vendor.qti.imsrtpservice@3.0 | \
             vendor.qti.ims.* | \
+            vendor.qti.latency* | \
             vendor.xiaomi.hardware.misys* | \
             vendor.qti.hardware.camera.device* | \
             vendor.nxp.*)
