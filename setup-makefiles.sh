2ff6f9dbbb6d93a885f86eec0da0bb2e4c935b0d
Richard Raya <rdxzv.dev@gmail.com>
2025-01-22 02:16:09 -0300

    surya: Fix vendor.qti.hardware.camera.device@1.0 ELF checks
    Change-Id: I7f8f6fe1915423daaf5b5a5bf011ca07b45146a6
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -51,6 +51,7 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.display.color* | \
             vendor.display.postproc@1.0 | \
             vendor.qti.data.* | \
+            vendor.qti.hardware.camera.device@1.0 | \
             vendor.qti.hardware.data.* | \
             vendor.qti.hardware.embmssl* | \
             vendor.qti.hardware.mwqemadapter@1.0 | \
