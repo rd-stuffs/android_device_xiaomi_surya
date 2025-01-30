b160719f112a0901aa668850f41a42ac7d1feff1
Richard Raya <rdxzv.dev@gmail.com>
2025-01-30 18:03:07 -0300

    surya: Switch adreno stack to LA.UM.9.1.r1-13700-SMxxx0.QSSI13.0
    Change-Id: If49662c03c821e65f225b5fa981ed88cfefb6d20
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,20 @@ $(call inherit-product, packages/apps/XiaomiParts/parts.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Adreno
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-1.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno \
+    ro.opengles.version=196610
+
 # Audio
 TARGET_PROVIDES_AUDIO_HAL := true
 
@@ -398,7 +412,6 @@ TARGET_BOARD_PLATFORM := $(MSMSTEPPE)
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
-    adreno \
     audio \
     av \
     bt \
